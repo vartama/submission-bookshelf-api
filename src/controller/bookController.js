307e11246cbca132cdb527faff032c0a237d0287
vartama <billylol9119@gmail.com>
2024-05-03 10:27:15 +0700

    feat: setup controller

---
+++ src/controller/bookController.js
@@ -0,0 +1,14 @@
+const books = require('../model/book.js');
+const {nanoid} = require('nanoid');
+
+const addBook = (request, h) => {}
+
+const getBooks = (request, h) => {}
+
+const detailBook = (request, h) => {}
+
+const editBook = (request, h) => {}
+
+const deleteBook = (request, h) => {}
+
+module.exports = { addBook, getBooks, detailBook, editBook, deleteBook }
